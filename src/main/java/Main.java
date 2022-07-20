6967f55e10bd746d0c20f88b20015b036caeb1bf
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-20 12:15:38 -0400

    implementation for sumOfDivisors()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,31 @@
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static int sumOfDivisors(int start, int end, int a, int b) {
-        // your code here
-        return 0;
+        return IntStream.rangeClosed(start, end)
+                .filter(number -> (number % a == 0) || (number % b == 0))
+                .sum();
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Please enter a start index: ");
         int start = scanner.nextInt();
+        System.out.print("Please enter a end index: ");
         int end = scanner.nextInt();
+        System.out.print("Please enter a divisor: ");
         int a = scanner.nextInt();
+        System.out.print("Please enter a divisor: ");
         int b = scanner.nextInt();
 
-        System.out.println(sumOfDivisors(start, end, a, b));
+        // sumOfDivisors(5, 15, 3, 5) -> 57
+        // sumOfDivisors(2, 8, 2, 3) -> 23
+        // System.out.println(sumOfDivisors(5, 15, 3, 5));
+        // System.out.println(sumOfDivisors(2, 8, 2, 3));
+
+        System.out.println("The sum of all divisors: " + sumOfDivisors(start, end, a, b));
 
         scanner.close();
     }
